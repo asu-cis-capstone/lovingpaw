705098c99ab5edf663c1196a7df9ab629a3c77b9
jrdespai <jrdespai@asu.edu>
2015-03-06 21:04:35 -0700

    Updated ImportSettings script - initial proof of concept

--- RecreationScript/ImportSettings.rb
+++ RecreationScript/ImportSettings.rb
@@ -15,6 +15,18 @@ browser["ShopifyLoginForm"].find_element(name: "button").click
 puts "Press Enter when the page has finished loading"
 gets
 browser.find_element(link_text: "Pages").click
+sleep(1)
+browser.find_element(link_text: "About Us").click
+sleep(1)
+browser["page-content_parent"].find_element(class: "rtetools-buttons").find_element(class: "tooltip-right-align").click
+#browser.switch_to.frame "page-content_ifr"
+sleep(1)
+browser["page-content"].clear
+file = File.open("ImportSettings/AboutUs.txt")
+#Txt file with html page layout of AboutUs
+pageContent = file.read
+browser["page-content"].send_keys(pageContent) 
+#browser.switch_to.default_content
 
-browser.close
+
 
